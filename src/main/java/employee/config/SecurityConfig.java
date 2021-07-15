2f97c2089f6318e50db23f38cbdfeb35b3912372
waqardetho9211 <waqaralidetho@hotmail.com>
2021-07-15 22:34:19 +0200

    Running except security

--- src/main/java/employee/config/SecurityConfig.java
+++ src/main/java/employee/config/SecurityConfig.java
@@ -2,19 +2,23 @@ package employee.config;
 
 import javax.annotation.security.DeclareRoles;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity(debug=true)
 @EnableGlobalMethodSecurity(jsr250Enabled = true)
-@DeclareRoles({"SERVICE","ACTUATOR"})
+@DeclareRoles({"ROLE_SERVICE","ROLE_ACTUATOR"})
 @ComponentScan(basePackages = "employee")
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
@@ -28,8 +32,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/", "/index", "/css/*", "/js/*").permitAll();
                 //.antMatchers("/api/*").hasRole("ACTUATOR")
 //                .antMatchers("/guests").hasRole("ACTUATOR")
-        http.authorizeRequests().antMatchers("/guests")
-                .access("hasRole('SERVICE') or hasRole('ACTUATOR')");
+        http.authorizeRequests().antMatchers("/guests").hasRole("ACTUATOR");
+//                .access("hasRole('SERVICE') or hasRole('ACTUATOR')");
                 //.anyRequest().hasRole("ACTUATOR")
 //                .anyRequest().authenticated()
         http.authorizeRequests()
@@ -41,26 +45,37 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .clearAuthentication(true)
                 .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
                 .logoutSuccessUrl("/logout-success").permitAll()
-//                .and()
-//                .httpBasic()
-//                .and()
-//                .jee().mappableRoles("ACTUATOR","SERVICE")
+                .and()
+                .httpBasic()
+                .and()
+                .jee().mappableRoles("ACTUATOR","SERVICE")
         ;
 
 
     }
 
-//    @Bean
-//    public UserDetailsService userDetailsService() {
-//        return new UserDetailsService() {
-//            @Override
-//            public UserDetails loadUserByUsername(String username) {
-//                    return new User(username, "",
-//                                    AuthorityUtils
-//                                            .commaSeparatedStringToAuthorityList("testing"));
-//            }
-//        };
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http
+//                .csrf().disable()
+//                .authorizeRequests()
+//                .antMatchers("/login", "/resources/**", "/j_security_check","css/**").permitAll()
+//                .anyRequest().authenticated()
+//                .and()
+//                .jee().mappableRoles("ACTUATOR", "SERVICE");
 //    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new UserDetailsService() {
+            @Override
+            public UserDetails loadUserByUsername(String username) {
+                    return new User(username, "",
+                                    AuthorityUtils
+                                            .commaSeparatedStringToAuthorityList("testing"));
+            }
+        };
+    }
 
 
 
